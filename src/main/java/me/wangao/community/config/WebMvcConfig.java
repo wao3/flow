9730041d31df7017d487f6e2de57f1b909d532c5
WangAo <wao1999@qq.com>
2021-01-31 11:02:09 +0800

    显示登录信息

---
+++ src/main/java/me/wangao/community/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package me.wangao.community.config;
+
+import me.wangao.community.controller.interceptor.LoginTicketInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LoginTicketInterceptor loginTicketInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/*/*.css", "/*/*.js", "/*/*.png", "/*/*.jpg", "/*/*.jpeg");
+    }
+}
---
+++ src/main/java/me/wangao/community/controller/interceptor/LoginTicketInterceptor.java
@@ -0,0 +1,56 @@
+package me.wangao.community.controller.interceptor;
+
+import me.wangao.community.entity.LoginTicket;
+import me.wangao.community.entity.User;
+import me.wangao.community.service.UserService;
+import me.wangao.community.util.CookieUtil;
+import me.wangao.community.util.HostHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class LoginTicketInterceptor implements HandlerInterceptor {
+
+    @Resource
+    private UserService userService;
+    
+    @Resource
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 从 cookie 中获取凭证
+        String ticket = CookieUtil.getValue(request, "ticket");
+        if (ticket != null) {
+            // 查询凭证
+            LoginTicket loginTicket = userService.findLoginTicket(ticket);
+            // 检查凭证
+            if (loginTicket != null && loginTicket.getStatus() == 0 && loginTicket.getExpired().after(new Date())) {
+                // 根据凭证查询用户
+                User user = userService.findUserById(loginTicket.getUserId());
+                // 在本次请求中持有用户
+                hostHolder.setUser(user);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if (user != null && modelAndView != null) {
+            modelAndView.addObject("loginUser", user);
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        hostHolder.clear();
+    }
+}
