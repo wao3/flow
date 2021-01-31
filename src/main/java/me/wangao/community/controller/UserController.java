6acf280f253ccc3bf212dc1572edbcd80726bd4f
WangAo <wao1999@qq.com>
2021-01-31 19:58:43 +0800

    账号上传头像功能

---
+++ src/main/java/me/wangao/community/controller/UserController.java
@@ -0,0 +1,107 @@
+package me.wangao.community.controller;
+
+import me.wangao.community.entity.User;
+import me.wangao.community.service.UserService;
+import me.wangao.community.util.CommunityUtil;
+import me.wangao.community.util.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private HostHolder hostHolder;
+
+    @GetMapping("/setting")
+    public String getSettingPage() {
+        return "/site/setting";
+    }
+
+    @PostMapping("/upload")
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        if(headerImage == null) {
+            model.addAttribute("error", "您还没有选择文件");
+            return "/site/setting";
+        }
+
+        String fileName = headerImage.getOriginalFilename();
+        assert fileName != null;
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if (StringUtils.isBlank(suffix)
+                || (!suffix.equals(".png") && !suffix.equals(".jpg") && !suffix.equals(".jpeg"))) {
+            model.addAttribute("error", "文件格式错误");
+            return "/site/setting";
+        }
+
+        // 生成随机文件名
+        fileName = CommunityUtil.generateUUID() + suffix;
+        // 确定文件路径
+        File dest = new File(uploadPath + "/" + fileName);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败: " + e.getMessage());
+            throw new RuntimeException("上传文件失败: 服务器发生异常", e);
+        }
+
+        // 更新用户头像的路径 （web路径）
+        // http://localhost:8080/community/user/header/xxx.png
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(), headerUrl);
+
+        return "redirect:/index";
+    }
+
+    @GetMapping("/header/{filename}")
+    public void getFile(@PathVariable String filename, HttpServletResponse res) {
+        // 服务器存放路径
+        filename = uploadPath + "/" + filename;
+        // 文件后缀
+        String suffix = filename.substring(filename.lastIndexOf(".")+1);
+        // 响应图片
+        res.setContentType("image/" + suffix);
+        try(FileInputStream fis = new FileInputStream(filename);
+            ServletOutputStream os = res.getOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("获取头像失败: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/me/wangao/community/service/UserService.java
+++ src/main/java/me/wangao/community/service/UserService.java
@@ -173,4 +173,8 @@ public class UserService implements CommunityConstant {
     public LoginTicket findLoginTicket(String ticket) {
         return loginTicketMapper.selectByTicket(ticket);
     }
+
+    public int updateHeader(int userId, String headerUrl) {
+        return userMapper.updateHeader(userId, headerUrl);
+    }
 }
