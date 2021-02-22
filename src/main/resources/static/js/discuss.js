e703297cb6e8f7fae052f49a1e1461745882942f
WangAo <wao1999@qq.com>
2021-02-22 22:07:31 +0800

    修复帖子详情

--- src/main/resources/static/js/discuss.js
+++ src/main/resources/static/js/discuss.js
@@ -3,7 +3,7 @@ function like(btn, entityType, entityId, entityUserId, postId) {
         data = JSON.parse(data);
         if (data.code === 0) {
             $(btn).children("i").text(data.likeCount);
-            $(btn).children("b").text(data.likeStatus === 1 ? "已赞" : "赞");
+            $(btn).children("b").text(data.likeStatus === 1 ? "已赞" : "点赞");
         } else {
             alert(data.msg)
         }
